06ba977299bfa723f43c87ac3c69b2769471d3a7
victoriashiel <vktorilocke@gmail.com>
2026-09-22 11:41:35 +0200

    brand: update wordmark to Shiel Accountants

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export function Logo() {
   return (
-    <Link href="/" className="wordmark" aria-label="Shiel home">
-      <span>SHIEL</span><span className="wordmark-dot" aria-hidden="true">.</span>
+    <Link href="/" className="wordmark" aria-label="Shiel Accountants home">
+      <span className="wordmark-main">SHIEL<span className="wordmark-dot" aria-hidden="true">.</span></span>
+      <span className="wordmark-sub">ACCOUNTANTS</span>
     </Link>
   );
 }
