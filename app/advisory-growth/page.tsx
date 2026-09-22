86de4d53edebdab5577110ad583f3f15bda260da
victoriashiel <vktorilocke@gmail.com>
2026-09-22 11:40:30 +0200

    feat: add advisory-growth page

---
+++ app/advisory-growth/page.tsx
@@ -0,0 +1,7 @@
+import { FocusServicePage, serviceMetadata } from "@/components/focus-service-page";
+
+export const metadata = serviceMetadata("advisory-growth");
+
+export default function Page() {
+  return <FocusServicePage slug="advisory-growth" />;
+}
