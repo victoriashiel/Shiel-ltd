f7ef33f9f6cb3dfa4707dda148a94df937b9bfe5
victoriashiel <vktorilocke@gmail.com>
2026-09-22 11:40:32 +0200

    feat: add international-accounting page

---
+++ app/international-accounting/page.tsx
@@ -0,0 +1,7 @@
+import { FocusServicePage, serviceMetadata } from "@/components/focus-service-page";
+
+export const metadata = serviceMetadata("international-accounting");
+
+export default function Page() {
+  return <FocusServicePage slug="international-accounting" />;
+}
