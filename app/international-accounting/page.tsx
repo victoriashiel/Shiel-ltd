f080d40af45d69d5bba8237b622f61b7ee7323d2
victoriashiel <vktorilocke@gmail.com>
2026-09-24 17:53:43 +0200

    Add cross-border scenarios to international accounting hub

--- app/international-accounting/page.tsx
+++ app/international-accounting/page.tsx
@@ -1,7 +1,13 @@
 import { FocusServicePage, serviceMetadata } from "@/components/focus-service-page";
+import { InternationalScenariosGrid } from "@/components/international-scenarios";
 
 export const metadata = serviceMetadata("international-accounting");
 
 export default function Page() {
-  return <FocusServicePage slug="international-accounting" />;
+  return (
+    <FocusServicePage
+      slug="international-accounting"
+      beforeCta={<InternationalScenariosGrid />}
+    />
+  );
 }
