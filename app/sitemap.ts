6d58e9b44b2310d346056016fd72aac4a8b0261f
victoriashiel <vktorilocke@gmail.com>
2026-09-24 17:18:10 +0200

    Add regional pages to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { services } from "@/lib/services";
 import { siteConfig } from "@/lib/site";
+import { regionContent } from "@/lib/region-content";
+import { regions, type RegionSlug } from "@/lib/regions";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
@@ -8,6 +10,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
     ...services.map((service) => ({
       url: `${siteConfig.url}/${service.slug}`,
     })),
+    ...Object.keys(regionContent).map((slug) => {
+      const region = regions[slug as RegionSlug];
+      return {
+        url: `${siteConfig.url}${region.path}`,
+        alternates: {
+          languages: {
+            [region.locale]: `${siteConfig.url}${region.path}`,
+            "x-default": siteConfig.url,
+          },
+        },
+      };
+    }),
     { url: `${siteConfig.url}/packages` },
     { url: `${siteConfig.url}/contact` },
     { url: `${siteConfig.url}/privacy` },
