b3e604b88ca990db7f4e6217bed6e32ff9325e2d
victoriashiel <vktorilocke@gmail.com>
2026-09-25 15:47:21 +0200

    Add payroll page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,6 +11,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     { url: `${siteConfig.url}/accounts` },
     { url: `${siteConfig.url}/tax` },
     { url: `${siteConfig.url}/bookkeeping` },
+    { url: `${siteConfig.url}/payroll` },
     { url: `${siteConfig.url}/packages` },
     ...["ireland", "united-kingdom", "gibraltar"].flatMap((country) => [
       { url: `${siteConfig.url}/packages/${country}` },
